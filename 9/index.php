273110aaad1212e4a0c0f95dd9aabd4ba5019b25
Nguyễn Tăng Vĩnh <33214648+nguyentangvinh@users.noreply.github.com>
2020-11-28 16:45:59 +0700

    module Vinh

---
+++ 9/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>10</title>
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/styles.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="anh">
+					<img src="./images/1.png">
+					<div class="overlay">
+					<a href="http://kamleshyadav.com/wp/fishing/seafood/wp-content/uploads/sites/4/2016/09/s4.png" data-rel="prettyPhoto"><i class="fa fa-plus"></i></a>
+				</div>
+				</div>
+				
+				<br>
+				<h5>BEEF STEW</h5>
+				<h5>
+				<span class="price">
+					<del>
+						<span class="price1">
+							<span class="woocommerce-Price-currencySymbol">£</span>
+							26.00
+						</span>
+					</del>
+					<ins>
+						<span class="price2">
+							<span class="woocommerce-Price-currencySymbol">£</span>
+							24.00
+						</span>
+					</ins>
+				</span>
+				</h5>
+				<br>
+				<button class="button button1"><i class="fa fa-shopping-cart">ADD TO CART</button></i></button>
+				
+			</div>
+			
+		</div>
+	</div>
+</body>
+</html>
