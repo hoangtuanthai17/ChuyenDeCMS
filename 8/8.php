a7109fe58a688fa4b5c65116f6c752090b4e833c
Nguyễn Tăng Vĩnh <33214648+nguyentangvinh@users.noreply.github.com>
2020-12-12 00:22:59 +0700

    module Vinh

---
+++ 8/8.php
@@ -0,0 +1,43 @@
+
+    
+
+    <?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+
+$less = new lessc;
+$less->compileFile('less/8.less', 'css/8.css');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>8</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/8.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <script src="https://kit.fontawesome.com/78e8255fc5.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/8.less', 'css/8.css');
+        ?>
+    </head>
+    <body >
+        <?php include '../8/8-content.php'; ?>
+    </body>
+</html>
